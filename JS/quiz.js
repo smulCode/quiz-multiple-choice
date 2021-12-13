db690e530d9af1e4e51bab654380c8070307df63
smulCode <67417862+smulCode@users.noreply.github.com>
2021-12-13 17:33:28 +0100

    refactor options

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,82 +1,49 @@
-
+const solutionOptions = [170, 18, 5, 2, 26];
 
 const quizLayout = () => {
-    //container quiz
-    const containerQuiz = document.createElement("div");
-    containerQuiz.classList.add("container-quiz");
+  //container quiz
+  const containerQuiz = document.createElement("div");
+  containerQuiz.classList.add("container-quiz");
 
-    //title
-    const genreTitle = document.createElement("div");
-    genreTitle.classList.add("quiz-title");
-    genreTitle.innerHTML = "Math Problem";
-    
+  //title
+  const genreTitle = document.createElement("div");
+  genreTitle.classList.add("quiz-title");
+  genreTitle.innerHTML = "Math Problem";
 
-    //quest counter
-    const questionCounter = document.createElement("div")
-    questionCounter.classList.add("question-counter");
-    questionCounter.innerHTML = "1/10";
-    //show question number instead of 1/10
+  //quest counter
+  const questionCounter = document.createElement("div");
+  questionCounter.classList.add("question-counter");
+  questionCounter.innerHTML = "1/6";
+  //show question number instead of 1/10
 
-    //question
-    const question = document.createElement("div")
-    question.classList.add("question");
-    question.innerHTML = "What is 21 - 16";
+  //question
+  const question = document.createElement("div");
+  question.classList.add("question");
+  question.innerHTML = "What is 21 - 16";
 
-    //multiple choice list
-    const solutions = document.createElement("ul")
-    solutions.classList.add("solutions");
-    
-    //multiple choice list items
-    const solutionOption1 = document.createElement("li")
-    solutionOption1.classList.add("solution-Option")
-    solutionOption1.innerHTML = "170";
+  //multiple choice list
+  const solutions = document.createElement("ul");
+  solutions.classList.add("solutions");
 
-    const solutionOption2 = document.createElement("li")
-    solutionOption2.classList.add("solution-Option")
-    solutionOption2.innerHTML = "18";
+  //multiple choice list items
 
-    const solutionOption3 = document.createElement("li")
-    solutionOption3.classList.add("solution-Option")
-    solutionOption3.innerHTML = "5";
+  solutionOptions.forEach((option) => {
+    const questionBar = document.createElement("li");
+    questionBar.classList.add("question-bar");
+    const questionText = document.createElement("p");
+    questionText.classList.add("question-text");
+    questionText.innerHTML = option;
 
-    const solutionOption4 = document.createElement("li")
-    solutionOption4.classList.add("solution-Option")
-    solutionOption4.innerHTML = "2";
+    solutions.append(questionBar);
+    questionBar.append(questionText);
+  });
 
-    const solutionOption5 = document.createElement("li")
-    solutionOption5.classList.add("solution-Option")
-    solutionOption5.innerHTML = "26";
-
-    //multiple Option list counter
-    // const solutionNumber1 = document.createElement("h3");
-    // solutionNumber1.classList.add("solution-number-left")
-    // solutionNumber1.innerHTML = "1";
-
-    // const solutionNumber2 = document.createElement("h3");
-    // solutionNumber2.classList.add("solution-number-right")
-    // solutionNumber2.innerHTML = "2";
-
-
-
-
-
-    
-
-    //add to container
-    game.append(containerQuiz);
-    containerQuiz.appendChild(genreTitle);
-    containerQuiz.appendChild(questionCounter);
-    containerQuiz.appendChild(question);
-    containerQuiz.appendChild(solutions);
-    // solutions.append(solutionNumber);
-    solutions.appendChild(solutionOption1);
-    solutions.appendChild(solutionOption2);
-    solutions.appendChild(solutionOption3);
-    solutions.appendChild(solutionOption4);
-    solutions.appendChild(solutionOption5);
-
-    
-
-}
+  //add to container
+  game.append(containerQuiz);
+  containerQuiz.appendChild(genreTitle);
+  containerQuiz.appendChild(questionCounter);
+  containerQuiz.appendChild(question);
+  containerQuiz.appendChild(solutions);
+};
 
 quizLayout();
