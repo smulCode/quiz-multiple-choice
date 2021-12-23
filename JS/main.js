93d425b1eb03dc0994a7e5732ab31c9dd7ca2106
smulCode <67417862+smulCode@users.noreply.github.com>
2021-12-23 20:48:38 +0100

    title

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,21 @@
 // import {quiz} from "./modules/quiz.js"
 const game = document.getElementById("game");
 const ButtonQuiz = () => {
+  const title = document.createElement("h1");
+  title.classList.add("h1-quiz");
+  title.innerText = "Math Quiz"
   const startBtn = document.createElement("button");
   startBtn.classList.add("start-btn");
   startBtn.innerText = "Start";
+  game.append(title);
   game.append(startBtn);
   startBtn.addEventListener("click", function () {
     (async () => {
       try {
         let app = await import("./modules/quiz.js");
-        console.log("startgame");
   
         startBtn.classList.add("display-none");
+        title.classList.add("display-none");
       } catch (error) {
         console.log(error);
       }
